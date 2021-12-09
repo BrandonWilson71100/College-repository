5737635539b1f400afab53c21cddb579848767a3
BrandonWilson71100 <36157002+BrandonWilson71100@users.noreply.github.com>
2021-12-09 01:43:29 +0000

    Camera and Movement
    I had an issue with the camera and movement that I previously had created and completely redone it from the ground up

--- New Unity Project/New Unity Project/Assets/MouseLook.cs
+++ New Unity Project/New Unity Project/Assets/MouseLook.cs
@@ -9,23 +9,29 @@ public class MouseLook : MonoBehaviour
 
     public Transform PlayerBody;
 
+    float xRotation = 0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
 
-        Transform.localRotation = quatermion.Euler(xRotation, 0f, 0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         PlayerBody.Rotate(Vector3.up * mouseX);
+
     }
 }
